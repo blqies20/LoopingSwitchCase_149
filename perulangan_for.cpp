0988980b5214caf22f240839428c7158f7188fa0
blqies20 <balqis1maharani@gmail.com>
2025-03-18 10:35:56 +0700

    perulangan for cacah naik

---
+++ perulangan_for.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int cacah;
+    //perulangan for untuk pencacah naik
+    for (cacah = 0; cacah <= 5; cacah++) {
+        cout << "Nilai Pencacahan = " << cacah << " Welcome " <<endl;
+    }
+    cout << "Nilai pencacahan terakhir = " << cacah;
+}
