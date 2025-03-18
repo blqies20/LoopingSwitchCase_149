dd6874c6a5b7beebbcfcaa52c59ba1bc8ab471b6
blqies20 <balqis1maharani@gmail.com>
2025-03-18 10:40:48 +0700

    perulangan for cacah turun

--- perulangan_for.cpp
+++ perulangan_for.cpp
@@ -7,5 +7,16 @@ int main(){
     for (cacah = 0; cacah <= 5; cacah++) {
         cout << "Nilai Pencacahan = " << cacah << " Welcome " <<endl;
     }
+    cout << endl;
+    cout << "Nilai pencacahan terakhir = " << cacah;
+
+    cout << endl;
+    cout << endl;
+
+    //perulangan for untuk pencacahan turun
+    for (cacah = 5; cacah >= 0; cacah--) {
+        cout << "Nilai Pencacahan = " << cacah << " Welcome " << endl;
+    }
+    cout << endl;
     cout << "Nilai pencacahan terakhir = " << cacah;
 }
