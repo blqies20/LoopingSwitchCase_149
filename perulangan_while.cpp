662a19911e9a7801013c81692e332e7387307955
blqies20 <balqis1maharani@gmail.com>
2025-03-18 11:46:26 +0700

    perulangan DoWhile

--- perulangan_while.cpp
+++ perulangan_while.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main(){
     int x;
     int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
     //Perulangan While
     cout << "Perulangan dengan while" << endl;
     srand(time(0));
@@ -18,4 +19,16 @@ int main(){
     }
     cout << "Nilai akhir x = " << x << endl;
     cout << "Perulangan while berlangsung sebanyak = " << perulanganWhile << endl;
+
+    cout << endl;
+
+    //Perulangan Do While
+    cout << "Perulangan dengan do while" << endl;
+    do{
+        cout << "Nilai x= " << x << " Lari keliling" << endl;
+        x = rand() % 10;
+        perulanganDoWhile++;
+    }while (x < 5);
+    cout << "Nilai akhir x = " << x << endl;
+    cout << "Perulangan do while berlangsung sebanyak = " << perulanganDoWhile << endl;
 }
